9e26afad6304dede46cac52636e52be41d829458
Rim2401 <chien7005@gmail.com>
2023-04-17 11:18:43 +0700

    first commit

---
+++ controller/SearchController.php
@@ -0,0 +1,31 @@
+<?php
+
+if (isset($_GET["act"])) {
+    $act = $_GET["act"];
+} else {
+    $act = '';
+}
+
+$thongbao = '';
+$error = '';
+$errorSearch = "";
+if (isset($_POST['search'])) {
+    $key = $_POST['key'];
+    if ($key == "") {
+        $errorSearch = "Vui lòng không để trống!";
+    }
+    $sql = "SELECT * FROM product WHERE name_product LIKE '%$key%'";
+    $sqlCount = "SELECT COUNT(*) FROM product WHERE name_product LIKE '%$key%'";
+    $searchProduct = selectAll($sql);
+    $rowProductSearch = selectRow($sqlCount);
+    if ($rowProductSearch == 0) {
+        $alert = "Không tìm thấy kết quả nào!";
+        $alert2 = "Hãy thử sử dụng các từ khóa chung chung hơn";
+    }
+    $product_category = loadCategory();
+    require_once('view/public/layout/header.php');
+    require_once('view/public/layout/search.php');
+    require_once('view/public/layout/footer.php');
+} else {
+    echo "Lỗi";
+}
---
+++ view/public/layout/contact.php
@@ -0,0 +1,19 @@
+<div class="container">
+        <div class="row" style="margin: 50px auto;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1557.3131610186756!2d108.24837889623096!3d15.97640267883965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142108997dc971f%3A0x1295cb3d313469c9!2sVietnam%20-%20Korea%20University%20of%20Information%20and%20Communication%20Technology.!5e0!3m2!1sen!2sus!4v1681638946438!5m2!1sen!2sus" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-xs-4">
+                <span style="font-size: 18px; font-weight: 600;"><i class="fas fa-map-marker-alt" style="background-color: #ff523b; color: #fff; width: 40px; height: 40px; text-align: center; font-size: 22px; padding-top: 8px;"></i> Địa Chỉ</span>
+                <p style="font-size: 15px; padding-top: 8px;">26 Huỳnh Văn Nghệ , Ngũ Hành Sơn , Đà Nẵng</p>
+            </div>
+            <div class="col-md-4 col-xs-4">
+                <span style="font-size: 18px; font-weight: 600;"><i class="fas fa-phone-alt" style="background-color: #ff523b; color: #fff; width: 40px; height: 40px; text-align: center; font-size: 22px; padding-top: 8px;"></i> Điện thoại</span>
+                <p style="font-size: 15px; padding-top: 8px;">033.4373.123 - Tư Vấn Khách Hàng</p>
+            </div>
+            <div class="col-md-4 col-xs-4">
+                <span style="font-size: 18px; font-weight: 600;"><i class="fas fa-envelope" style="background-color: #ff523b; color: #fff; width: 40px; height: 40px; text-align: center; font-size: 22px; padding-top: 8px;"></i> Email</span>
+                <p style="font-size: 15px; padding-top: 8px;">Vux.0511@gmail.com</p>
+            </div>
+        </div>
+    </div>
